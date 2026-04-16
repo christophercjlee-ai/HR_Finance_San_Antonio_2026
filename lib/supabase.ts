cd0b23ab4a1cb528b6a094b9575a81dab5772caa
christophercjlee-ai <christopher.cj.lee@gmail.com>
2026-04-16 18:33:16 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
 
 const SUPABASE_URL = "https://fwptxtnbknfknxuxxwek.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cHR4dG5ia25ma254dXh4d2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NzU5MzksImV4cCI6MjA5MDA1MTkzOX0.bFz6oQx3dh8TDiICFSyVMAJsMhHB4EU5oWTWlMx44kc";
@@ -10,6 +11,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: Platform.OS === "web",
   },
 });
